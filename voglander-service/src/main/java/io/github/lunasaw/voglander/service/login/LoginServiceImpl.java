6ce4f7b1fa57230a59c95b0750c64c0c4fc98bf1
chenzhangyue <chenzhangyue01@weidian.com>
2023-12-30 00:21:36 +0800

    :package: [feat] 日志打印

--- voglander-service/src/main/java/io/github/lunasaw/voglander/service/login/LoginServiceImpl.java
+++ voglander-service/src/main/java/io/github/lunasaw/voglander/service/login/LoginServiceImpl.java
@@ -1,19 +1,21 @@
 package io.github.lunasaw.voglander.service.login;
 
 import io.github.lunasaw.voglander.client.service.LoginService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
  * @author luna
  * @date 2023/12/29
  */
+@Slf4j
 @Service
 public class LoginServiceImpl implements LoginService {
 
 
     @Override
     public void login() {
-
+        log.info("login::");
     }
 
     @Override
