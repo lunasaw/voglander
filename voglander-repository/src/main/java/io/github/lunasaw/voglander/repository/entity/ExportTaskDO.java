14ed6ca8e019363562172d6c3aaa2d1ffc753161
chenzhangyue <chenzhangyue01@weidian.com>
2024-01-29 22:22:59 +0800

    :package: [feat] task type

--- voglander-repository/src/main/java/io/github/lunasaw/voglander/repository/entity/ExportTaskDO.java
+++ voglander-repository/src/main/java/io/github/lunasaw/voglander/repository/entity/ExportTaskDO.java
@@ -1,7 +1,15 @@
 package io.github.lunasaw.voglander.repository.entity;
 
+import java.io.Serializable;
 import java.util.Date;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import io.github.lunasaw.voglander.common.enums.export.ExportTaskTypeEnums;
+import io.github.lunasaw.voglander.common.enums.export.ExportTaskStatusEnum;
 import lombok.Data;
 
 /**
@@ -11,72 +19,77 @@ import lombok.Data;
  * @since 2024-01-26 15:21:45
  */
 @Data
-public class ExportTaskDO {
-
+@TableName("tb_export_task")
+public class ExportTaskDO implements Serializable {
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
     /**
      * ID自增
      */
-    private Long    id;
+    @TableId(type = IdType.AUTO)
+    private Long              id;
     /**
      * 创建时间
      */
-    private Date    gmtCreate;
+    private Date              gmtCreate;
     /**
      * 更新时间
      */
-    private Date    gmtUpdate;
+    private Date              gmtUpdate;
     /**
      * 任务唯一Id
      */
-    private Long    bizId;
+    private Long              bizId;
     /**
      * 导出的客户总数
      */
-    private Long    memberCnt;
+    private Long              memberCnt;
     /**
      * 文件格式
      */
-    private String  format;
+    private String            format;
     /**
      * 申请时间
      */
-    private Date    applyTime;
+    private Date              applyTime;
     /**
      * 导出报表时间
      */
-    private Date    exportTime;
+    private Date              exportTime;
     /**
      * 文件下载地址, 多个url用、隔开
      */
-    private String  url;
+    private String            url;
     /**
+     * {@link ExportTaskStatusEnum}
      * 是否完成，1 -> 完成, 0->处理中, -1 -> 出错
      */
-    private Integer status;
+    private Integer           status;
     /**
      * 是否过期，1 -> 过期，0 -> 未过期
      */
-    private Integer expired;
+    private Integer           expired;
     /**
      * 是否删除，1 -> 删除, 0 -> 未删除
      */
-    private Integer deleted;
+    private Integer           deleted;
     /**
      * 搜索条件序列化
      */
-    private String  param;
+    private String            param;
 
     /**
      * 导出名称
      */
-    private String  name;
+    private String            name;
 
     /**
-     * 导出类型 1付费会员卡
+     * {@link ExportTaskTypeEnums}
+     * 导出类型
      */
-    private Integer type;
+    private Integer           type;
 
-    private String  applyUser;
+    private String            applyUser;
 
-    private String  extend;
+    private String            extend;
 }
