bdbc105979ad9cbd3335af895782f0bdfcb88365
chenzhangyue <chenzhangyue01@weidian.com>
2024-02-20 17:04:38 +0800

    :package: [feat] code

--- voglander-test/src/test/java/io/github/lunasaw/app/ApiTest.java
+++ voglander-test/src/test/java/io/github/lunasaw/app/ApiTest.java
@@ -211,4 +211,77 @@ public class ApiTest {
         connection.close();
         connection1.close();
     }
+
+    /**
+     * 三、Repeatable Read 【重复读】
+     * MySql默认该隔离级别
+     * 该隔离级别可以让事务在自己的会话中重复读取数据 并且不会出现结果不一样的状况
+     * 即使其他事务已经提交了 也依然还是显示以前的数据
+     *
+     * 例：
+     *
+     * 1、开启一个命令行窗口A开始事务 然后查询表中记录
+     * 设置当前窗口的事务隔离级别为重复读
+     * 命令：
+     *
+     * set session transaction isolation level repeatable read
+     * 1.
+     * 2、另外再打开一个窗口B 开启事务 然后执行sql语句 但不提交
+     * 3、在A窗口重新执行查询 会看到B窗口刚才执行sql语句的结果 因为它还没有提交
+     * 4、在B窗口执行提交
+     * 5、在A窗口中执行查看 此时查询结果 结果会和以前的查询结果一致 不会发生改变
+     * -----------------------------------
+     * mysql 设置读未提交的事务隔离级别执行无锁读 mysql 读已提交
+     * https://blog.51cto.com/u_16213613/7781724
+     */
+    @Test
+    public void atest_设置隔离级别_可重复读() throws SQLException {
+        // B窗口 修改不提交
+        Connection connection = dataSource.getConnection();
+        connection.setAutoCommit(false);
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("set session transaction isolation level repeatable read");
+
+        ResultSet resultSet = statement.executeQuery("select @@transaction_isolation");
+
+        while (resultSet.next()) {
+            String string = resultSet.getString(1);
+            System.out.println(string);
+        }
+        statement.executeUpdate("UPDATE tb_device SET `status` = 2 WHERE device_id = '33010602011187000001';");
+
+        Connection connection1 = dataSource.getConnection();
+        connection1.setAutoCommit(false);
+        Statement statement1 = connection1.createStatement();
+        // 设置隔离级别为读未提交
+        statement1.executeUpdate("set session transaction isolation level repeatable read");
+
+        ResultSet resultSet2 = statement1.executeQuery("select @@transaction_isolation");
+
+        while (resultSet2.next()) {
+            String string = resultSet2.getString(1);
+            System.out.println(string);
+        }
+
+        // A 窗口 查询
+        resultSet2 = statement1.executeQuery("SELECT * from tb_device where device_id = '33010602011187000001';");
+        while (resultSet2.next()) {
+            // 读到1
+            String string = resultSet2.getString(6);
+            System.out.println(string);
+        }
+
+        // B 提交
+        connection.commit();
+
+        resultSet2 = statement1.executeQuery("SELECT * from tb_device where device_id = '33010602011187000001';");
+        while (resultSet2.next()) {
+            // A读到还是1
+            String string = resultSet2.getString(6);
+            System.out.println(string);
+        }
+
+        connection.close();
+        connection1.close();
+    }
 }
